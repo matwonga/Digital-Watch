c31730ee5881eadb4357c5d523d66849f43dba31
matwonga <mat.wonglh@gmail.com>
2026-05-22 07:51:00 +0000

    Arming Latch

---
+++ rtl/arming_latch.sv
@@ -0,0 +1,30 @@
+// arming_latch - A latch to hold the armed state of the system
+//
+// Parameters:
+// None
+//
+// Ports:
+// clk    - Clock signal
+// arm    - Signal to set the latch
+// disarm - Signal to reset the latch
+// armed  - Output signal indicating whether the system is armed
+
+`timescale 1ns / 1ps
+
+module arming_latch (
+    input  logic clk,
+    input  logic arm,
+    input  logic disarm,
+    output logic armed = 1'b0
+);
+
+  always_ff @(posedge clk) begin
+    if (disarm) begin
+      armed <= 1'b0;
+    end else if (arm) begin
+      armed <= 1'b1;
+    end
+  end
+
+endmodule
+
