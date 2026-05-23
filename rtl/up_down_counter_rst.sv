40be90ca1bdb818838d80d7ccdac54bc2a39ad08
matwonga <mat.wonglh@gmail.com>
2026-05-23 18:02:23 +0000

    Up Down Counter with Reset

---
+++ rtl/up_down_counter_rst.sv
@@ -0,0 +1,42 @@
+// up_down_counter - display decoder for hexadecimal digits
+//
+// Parameters:
+// MAX    - Maximum value for the counter
+// WIDTH  - Binary width of the max value for the counter
+//
+// Ports:
+// clk                - Clock signal
+// rst                - Synchronous reset signal
+// enable             - When high, enables the counter
+// up                 - When high count increments, when low count decrements
+// count [WIDTH-1:0]  - Counter outputs
+
+`timescale 1ns / 1ps
+
+module up_down_counter_rst #(
+    parameter int WIDTH = 2,
+    parameter logic [WIDTH-1:0] MAX = 2
+) (
+    input logic clk,
+    input logic rst,
+    input logic enable,
+    input logic up,
+    output logic [WIDTH-1:0] count = '0
+);
+
+  localparam logic [WIDTH-1:0] Max = WIDTH'(MAX);
+  localparam logic [WIDTH-1:0] One = WIDTH'(1);
+
+  always_ff @(posedge clk) begin
+    if (rst) begin
+      count <= '0;
+    end else if (enable) begin
+      if (up) begin
+        count <= (count == Max) ? 0 : count + One;
+      end else begin
+        count <= (count == 0) ? Max : count - One;
+      end
+    end
+  end
+
+endmodule
